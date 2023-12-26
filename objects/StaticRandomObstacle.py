39dc11a6886f3771faa46892d507d0b663fd50b1
bison-- <mike.bison42@googlemail.com>
2023-12-26 04:24:26 +0100

    more obstacles

--- objects/StaticRandomObstacle.py
+++ objects/StaticRandomObstacle.py
@@ -6,13 +6,16 @@ from objects.BaseObject import BaseObject
 class StaticRandomObstacle(BaseObject):
     def __init__(self, x, y):
         sprites = [
+            os.path.join('images', 'pine_tree.png'),
+            os.path.join('images', 'pine_tree_snow.png'),
             os.path.join('images', 'pine_trees.png'),
             os.path.join('images', 'rocks.png'),
+            os.path.join('images', 'wooden_hut_1.png'),
         ]
 
         random_sprites = random.choices(
             sprites,
-            weights=(50, 10),
+            weights=(50, 25, 50, 10, 5),
             k=1
         )
 
